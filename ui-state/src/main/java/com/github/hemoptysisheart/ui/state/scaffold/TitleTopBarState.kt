9f4205d2f8a13a4402cfaebbefe33ca5a6ae4b1e
H2 <hemoptysisheart@gmail.com>
2024-05-10 23:00:53 +0900

    🏷️ 단순 문자 표시용 상태 홀더 추가.

--- ui-state/src/main/java/com/github/hemoptysisheart/ui/state/scaffold/TitleTopBarState.kt
+++ ui-state/src/main/java/com/github/hemoptysisheart/ui/state/scaffold/TitleTopBarState.kt
@@ -1,11 +1,18 @@
 package com.github.hemoptysisheart.ui.state.scaffold
 
 import androidx.compose.runtime.Stable
+import com.github.hemoptysisheart.ui.state.TextState
 import java.util.UUID
 
 @Stable
 data class TitleTopBarState(
-    val title: String,
+    val title: TextState,
     override val key: UUID = UUID.randomUUID(),
     override val testTag: String = key.toString()
-) : TopBarState
+) : TopBarState {
+    constructor(
+        title: String,
+        key: UUID = UUID.randomUUID(),
+        testTag: String = key.toString()
+    ) : this(TextState(title), key, testTag)
+}
