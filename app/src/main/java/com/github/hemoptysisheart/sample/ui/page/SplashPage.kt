b827b6091f68d5310ea33b684b3e1e55c4f7b78e
H2 <hemoptysisheart@gmail.com>
2024-05-14 22:33:59 +0900

    🏷️ 스플래시 화면에 뷰모델 추가.
    1. 백그라운드 작업에 의존적인 경우.
    2. 앱 상태에 따른 분기가 필요한 경우 : 튜토리얼 표시 등.

--- app/src/main/java/com/github/hemoptysisheart/sample/ui/page/SplashPage.kt
+++ app/src/main/java/com/github/hemoptysisheart/sample/ui/page/SplashPage.kt
@@ -6,28 +6,51 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.hemoptysisheart.sample.ui.navigation.SplashNavigator
 import com.github.hemoptysisheart.sample.ui.theme.AndroidLibraryTheme
-import kotlinx.coroutines.delay
+import com.github.hemoptysisheart.sample.viewmodel.SplashViewModel
+import com.github.hemoptysisheart.ui.navigation.compose.baseViewModel
 
 @Composable
-fun SplashPage(navigator: SplashNavigator) {
+fun SplashPage(navigator: SplashNavigator, viewModel: SplashViewModel = baseViewModel()) {
     Log.v(TAG, "#SplashPage called.")
 
-    LaunchedEffect(SplashNavigator::class) {
-        delay(3_000)
-        navigator.selectSize()
-    }
+    var stay by remember { mutableStateOf(true) }
 
-    SplashPageContent()
+    val timeout by viewModel.timeout.collectAsStateWithLifecycle()
+
+    SplashPageContent(
+        timeout = timeout,
+        onTimeout = {
+            if (stay) {
+                stay = false
+                navigator.selectSize()
+            }
+        }
+    )
 }
 
+/**
+ * @param timeout Splash 화면 표시 시간이 끝났는지 여부.
+ * @param onTimeout Splash 화면 표시 시간이 끝났을 때 호출할 함수.
+ */
 @Composable
-private fun SplashPageContent() {
+private fun SplashPageContent(
+    timeout: Boolean,
+    onTimeout: () -> Unit = {}
+) {
+    if (timeout) {
+        onTimeout()
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -41,6 +64,6 @@ private fun SplashPageContent() {
 @Preview(showSystemUi = true)
 private fun SplashPagePreview() {
     AndroidLibraryTheme {
-        SplashPageContent()
+        SplashPageContent(false)
     }
 }
